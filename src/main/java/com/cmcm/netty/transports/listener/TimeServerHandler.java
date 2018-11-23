6bd0ceb212636a023ab2c88c581474893360a4a5
Spontaneously <hongfei@cmcm.com>
2018-11-23 20:38:17 +0800

    Merge branch 'master' of https://github.com/spontaneously5201314/netty_study

---
+++ src/main/java/com/cmcm/netty/transports/listener/TimeServerHandler.java
@@ -0,0 +1,33 @@
+package com.cmcm.netty.transports.listener;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author Spontaneously
+ * @date 2018/9/17 19:34
+ **/
+public class TimeServerHandler extends ChannelHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        final ByteBuf time = ctx.alloc().buffer(4);
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+        final ChannelFuture f = ctx.writeAndFlush(time);
+        f.addListener(future -> {
+            System.out.println("接受完成");
+            ctx.close();
+        });
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
